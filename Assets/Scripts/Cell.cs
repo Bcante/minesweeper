2abf45101922914a69c6099d6212d72c0b394f90
Benoît Cante <cantebenoit@hotmail.com>
2021-03-29 08:33:01 +0200

    first commit

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections.Generic;
+
+public class Cell : MonoBehaviour
+{
+    public int x, y;
+    public int minesInNeighborhood;
+    public bool isMine;
+    public bool isFlag;
+    public Sprite[] sprites;
+    public bool revealed;
+    public List<GameObject> adjacentCells;
+    public Dictionary<String, GameObject> dict;
+
+
+    private void Awake()
+    {
+        adjacentCells = new List<GameObject>();
+    }
+
+    public void updatesMinesInNeighborhood()
+    {
+        for (int i = (x - 1); i < x + 2; i++)
+        {
+            for (int j = (y - 1); j < y + 2; j++)
+            {
+                bool inBound = Testing.isInBound(i, j);
+                if (inBound && (i != x || j != y))
+                {
+                    if (Testing.cells[i, j].GetComponent<Cell>().isMine)
+                    {
+                        this.minesInNeighborhood++;
+                    }
+                    adjacentCells.Add(Testing.cells[i, j]);
+                }
+            }
+        }
+
+        GetComponent<SpriteRenderer>().sprite = sprites[11]; // J'ai honte mais Martin à dit ok, et ça c'est le sprite par défaut si vous vous demandez
+
+    }
+
+    internal void plantMine()
+    {
+        isMine = true;
+    }
+
+    public void SetCoordinates(int i, int j)
+    {
+        x = i;
+        y = j;
+    }
+    public void Init()
+    {
+        isMine = false;
+        minesInNeighborhood = 0;
+    }
+
+    public void Reveal()
+    {
+        revealed = true;
+        if (!isFlag)
+        {
+            if (isMine)
+            {
+                GetComponent<SpriteRenderer>().sprite = sprites[10]; // Sprite de isMine
+                Testing.gameLost = true;
+            }
+            else if (minesInNeighborhood != 0)
+            {
+                GetComponent<SpriteRenderer>().sprite = sprites[minesInNeighborhood]; // J'ai honte mais Martin à dit ok
+            }
+            else
+            {
+                RevealNearbyTiles();
+                //pour tous les voisins minesInNeighborhood 0 on tente de les révéler mais bon à coup sûr je vais me taper une boucle inifinie pfff nique la prog            
+            }
+        }
+    }
+
+    private void RevealNearbyTiles()
+    {
+        GetComponent<SpriteRenderer>().sprite = sprites[minesInNeighborhood]; // J'ai honte mais Martin à dit ok
+        foreach (var item in adjacentCells)
+        {
+            Cell c = item.GetComponent<Cell>();
+            if (!isMine && !c.revealed)
+            {
+                c.revealed = true;
+                if (c.minesInNeighborhood == 0) // Si le voisin en question n'a pas de isMinee dans son voisinage ET si on l'a pas encore révélé. 
+                {
+                    c.RevealNearbyTiles();
+                }
+                else
+                {
+                    c.GetComponent<SpriteRenderer>().sprite = sprites[c.GetComponent<Cell>().minesInNeighborhood];
+                }
+            }
+          
+        }
+    }
+
+    public void FlagCell()
+    {
+        // Test si on peut isFlag. On peut que si c'est pas encore revelé
+        if (!revealed)
+        {
+            if (isFlag)
+                GetComponent<SpriteRenderer>().sprite = sprites[11]; // J'ai honte mais Martin à dit ok
+            else
+                GetComponent<SpriteRenderer>().sprite = sprites[9];
+            isFlag = !isFlag;
+        }
+    }
+}
---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    
+    public GameObject placeHolder;
+
+    public static int GRID_SIZE = 10;
+
+    public static GameObject[,] cells = new GameObject[GRID_SIZE,GRID_SIZE];
+    public static bool gameLost;
+
+    public static float offsetX = .5f;
+    public static float offsetY = .5f;
+
+    void Awake()
+    {
+        System.Random ran = new System.Random();
+        for (int i = 0; i < GRID_SIZE; i++)
+        {
+            for (int j = 0; j < GRID_SIZE; j++)
+            {
+                GameObject g = Instantiate(placeHolder, new Vector2(i + Testing.offsetX, j + Testing.offsetY), Quaternion.identity) ;
+                
+                Cell c = g.GetComponent<Cell>();
+
+                c.Init();
+                c.SetCoordinates(i, j);
+                cells[i, j] = g;
+                
+            }
+        }
+
+        for (int i2 = 0; i2 < GRID_SIZE; i2++)
+        {
+            for (int j2 = 0; j2 < GRID_SIZE; j2++)
+            {
+                int r = ran.Next(0, 100);
+                bool bomb = (r <= 15);
+                if (bomb && ((i2!= 4) && (j2!=4)))
+                {
+                    cells[i2, j2].GetComponent<Cell>().plantMine();
+                }
+            }
+        }
+        //cells[4, 4].GetComponent<Cell>().plantMine();
+        for (int i = 0; i < GRID_SIZE; i++)
+        {
+            for (int j = 0; j < GRID_SIZE; j++)
+            {
+                cells[i, j].GetComponent<Cell>().updatesMinesInNeighborhood();
+                Debug.Log(cells[i, j].GetComponent<Cell>().adjacentCells.Count);
+            }
+        }
+    }
+
+
+    void Update()
+    {
+        Cell c = default; // ça c'est marrant
+        if (Input.GetMouseButtonDown(0) ||  Input.GetMouseButtonDown(1)) {
+            if (!gameLost)
+            {
+                var v3 = Input.mousePosition;
+                v3 = Camera.main.ScreenToWorldPoint(v3);
+                int x, y;
+                x = (int)Math.Truncate(v3.x);
+                y = (int)Math.Truncate(v3.y);
+
+                if (isInBound(x, y))
+                {
+                    c = cells[x, y].GetComponent<Cell>();
+                }
+
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    c.Reveal();
+                }
+                if (Input.GetButtonDown("Fire2"))
+                {
+                    c.FlagCell();
+                }
+            }
+        }
+    }
+        public static bool isInBound(int x, int y)
+    {
+        if (x >= 0 && x < Testing.GRID_SIZE && y >= 0 && y < Testing.GRID_SIZE) return true; else return false;
+    }
+
+    private void firstClick()
+    {
+        /*
+         * Le premier clic doit enlever la bomb si il y en a une
+         * il va ensuite chercher une direction
+         * */
+    }
+}
